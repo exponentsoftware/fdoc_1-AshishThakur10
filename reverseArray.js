930fc15f119d5a9d52bf328adbda55f7dbe754fa
AshishThakur10 <sendashishthakur@gmail.com>
2021-07-23 20:52:12 +0530

    reverse array

---
+++ reverseArray.js
@@ -0,0 +1,12 @@
+/*
+2c. Reversing an array: Arrays have a reverse method which changes the array by inverting the order in which its elements appear. For this exercise, write a function, reverseArray. The  reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order. Without reverse method.
+```js
+console.log(reverseArray(["A", "B", "C"]));
+["C", "B", "A"]
+```
+*/
+
+const reverseArray = ["C", "B", "A"];
+reverseArray.reverse();
+
+console.log(reverseArray);
